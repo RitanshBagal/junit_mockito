1520d9215a8421a0613dc6522127ded80d30bdd1
ritanshbagal <ritanshbagal@gmail.com>
2025-01-29 00:17:37 +0530

    mock

--- src/test/java/business/TodoBusinessMockTest.java
+++ src/test/java/business/TodoBusinessMockTest.java
@@ -4,16 +4,21 @@ import data.api.TodoService;
 import data.api.TodoServiceStub;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
 
 public class TodoBusinessMockTest {
     @Test
     public void testRetrieveTodosRelatedToSpring_usingMock(){
-        TodoService todoServiceStub = new TodoServiceStub();
-        TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoServiceStub);
-        List<String> filteredTodos = todoBusinessImpl.retrieveTodosRelatedToSpring("dummy");
+
+        TodoService todoServiceMock = mock(TodoService.class);
+        List<String> todos = Arrays.asList("Learn Spring MVC", "Learn Spring", "Learn to Code");
+        when(todoServiceMock.retrieveTodos("Dummy")).thenReturn(todos);
+        TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoServiceMock);
+        List<String> filteredTodos = todoBusinessImpl.retrieveTodosRelatedToSpring("Dummy");
         assertEquals(2, filteredTodos.size());
     }
 }
